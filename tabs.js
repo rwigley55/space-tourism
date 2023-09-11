67026c8d02d5dc5a5c53166796bc59e54c602202
Reed Wigley <rwigley55@gmail.com>
2023-09-11 11:00:37 -0700

    Add tabs for crew page

--- tabs.js
+++ tabs.js
@@ -17,26 +17,27 @@ function changeTabFocus(e) {
   // change the tabindex of the current tab to -1
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1);
-  }
-  //if the right key is pushed, move to the next tab on the right
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    // if tabFocus becomes larger than 4, then set tabFocus back to 0
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
-    }
-  }
-  //if the left key is pushed, move to the next tab on the left
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
-  }
 
-  //Can be focused on, but doesn't set the focus immediately
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
+    //if the right key is pushed, move to the next tab on the right
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      // if tabFocus becomes larger than 4, then set tabFocus back to 0
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    }
+    //if the left key is pushed, move to the next tab on the left
+    else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
+    }
+
+    //Can be focused on, but doesn't set the focus immediately
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+  }
 }
 
 function changeTabPanel(e) {
@@ -48,17 +49,37 @@ function changeTabPanel(e) {
   const tabContainer = targetTab.parentNode;
   const mainContainer = tabContainer.parentNode;
 
-  mainContainer
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((panel) => panel.setAttribute("hidden", true));
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
 
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+  targetTab.setAttribute("aria-selected", true);
 
-  mainContainer
-    .querySelectorAll("picture")
-    .forEach((picture) => picture.setAttribute("hidden", true));
+  hideContent(mainContainer, '[role="tabpanel"]');
+  // mainContainer
+  //   .querySelectorAll('[role="tabpanel"]')
+  //   .forEach((panel) => panel.setAttribute("hidden", true));
 
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+  showContent(mainContainer, [`#${targetPanel}`]);
+  // mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
 
-  //   console.log(targetPanel);
+  hideContent(mainContainer, "picture");
+  // mainContainer
+  //   .querySelectorAll("picture")
+  //   .forEach((picture) => picture.setAttribute("hidden", true));
+
+  showContent(mainContainer, [`#${targetImage}`]);
+  // mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+
+  //  console.log(targetPanel);
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden");
 }
